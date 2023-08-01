1749ccd1b5d47b2f09fa3deaeec57fce7931a3d6
Xander Cole <acole67@uncc.edu>
2023-08-01 19:36:22 -0400

    Updated navbar format

--- src/pages/components/Navbar.tsx
+++ src/pages/components/Navbar.tsx
@@ -10,22 +10,23 @@ export default function Navbar() {
   const pathname = usePathname();
   return (
     <>
-      <div className="absolute top-1 mt-8 px-4">
-        <Link href="/">
-          <Image
-            className="hover:cursor-pointer"
-            width={350}
-            // layout="responsive"
-            unoptimized={true}
-            height={350}
-            src={"/Roboverse_Animation.gif"}
-            alt="VESL Logo"
-          ></Image>
-        </Link>
-      </div>
-      <div className="border-b-2 border-slate-700 pb-4">
-        <div className="mt-5 flex flex-row items-center justify-between">
-          <ul className=" flex grow snap-center flex-row items-center justify-center ">
+      <div className="h-24 border-b-2">
+        <div className="absolute top-11 px-4">
+          <Link href="/">
+            <Image
+              className="hover:cursor-pointer"
+              width={350}
+              // layout="responsive"
+              unoptimized={true}
+              height={350}
+              src={"/Roboverse_Animation.gif"}
+              alt="VESL Logo"
+            ></Image>
+          </Link>
+        </div>
+
+        <div className="relative mt-5 flex flex-row items-center justify-between">
+          <ul className=" absolute inset-0 top-11 flex  flex-row items-center justify-center ">
             {navPages.map((page) => {
               return (
                 <li key={null}>
@@ -37,10 +38,10 @@ export default function Navbar() {
               );
             })}
           </ul>
+        </div>
 
-          <div className="px-4">
-            <ProfilePicture width={80} height={80}></ProfilePicture>
-          </div>
+        <div className="absolute right-2 top-5 px-4">
+          <ProfilePicture width={80} height={80}></ProfilePicture>
         </div>
       </div>
     </>
