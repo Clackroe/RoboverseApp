66ce2b532f52f95a707ec7369199cd58f3ffac24
Xander Cole <acole67@uncc.edu>
2023-09-13 14:11:39 -0400

    feat: banner to veslverse

--- src/pages/components/Layout.tsx
+++ src/pages/components/Layout.tsx
@@ -2,12 +2,21 @@ import React, { type ReactNode } from "react";
 import { signIn, useSession } from "next-auth/react";
 import Image from "next/image";
 import Navbar from "./Navbar";
+// import ReactDOM from "react-dom";
 
 const Layout = ({ children }: { children: ReactNode }) => {
   const { data: sessionData } = useSession();
 
   return (
     <>
+      <a href="https://app.vesl.gg/catalog">
+        <div className="vi hover:border-b-full mx-96 rounded-b-full bg-gradient-to-br from-green-200 via-green-500 to-green-800 py-2  text-black transition-all hover:mx-[750px] hover:underline">
+          <div className="flex items-center justify-center ">
+            <p className=" ">MORE IN THE VESLVERSE</p>
+          </div>
+        </div>
+      </a>
+
       <div className="overflow-x-hidden overscroll-x-none">
         {sessionData ? <Navbar /> : <Authenticate />}
         {sessionData ? children : null}
